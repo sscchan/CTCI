4f682ebc87981cd0d615dbaebe81569402db7897
sscchan <stephen.s.c.chan@gmail.com>
2017-03-21 10:22:54 -0700

    add comments on time comlexity

--- 1.2_CheckPermutation/1.2_CheckPermutation.js
+++ 1.2_CheckPermutation/1.2_CheckPermutation.js
@@ -1,6 +1,14 @@
 // 1.2 Check Permutation
 // Given two strings, check if one is a permutation of another
 function checkPermutation(string1, string2) {
+
+  // Split - O(n)
+  // Sort - O(n log n)
+  // Join - O(n)
+  // === Compare - O(n)
+  // ----------------------
+  // Actual: O(n log n)
+
   return (string1.split('').sort().join('') === string2.split('').sort().join(''));
 }
 
@@ -8,21 +16,22 @@ function checkPermutationOptimised(string1, string2) {
   var string1CharacterCount = {};
   var string2CharacterCount = {};
 
-  countStringCharacters(string1, string1CharacterCount);
-  countStringCharacters(string2, string2CharacterCount);
+  countStringCharacters(string1, string1CharacterCount); // O(n of string1)
+  countStringCharacters(string2, string2CharacterCount); // O(n of string2)
 
-  // Test to see if both strings has the same number of different "characters"
+  // Test to see if both strings has the same number of different "characters" // O(1)
   if (Object.keys(string1CharacterCount).length !== Object.keys(string2CharacterCount).length) {
     return false;
   }
 
-  // Test to see if both string has the same number of each "characters"
+  // Test to see if both string has the same number of each "characters" // O(n)
   for (key in string1CharacterCount) {
     if (string1CharacterCount[key] !== string2CharacterCount[key]) {
       return false;
     }
   }
 
+  // Time complexity O(n)
   return true;
 
   function countStringCharacters(string, counts) {
