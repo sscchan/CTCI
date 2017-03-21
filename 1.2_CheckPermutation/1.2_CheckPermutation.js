0889395862600888655b792694215f30b70b2e10
sscchan <stephen.s.c.chan@gmail.com>
2017-03-21 09:56:09 -0700

    add function and test skeletons

---
+++ 1.2_CheckPermutation/1.2_CheckPermutation.js
@@ -0,0 +1,11 @@
+// 1.2 Check Permutation
+// Given two strings, check if one is a permutation of another
+function checkPermutation(string1, string2) {
+
+}
+
+function testCode() {
+
+}
+
+testCode();
