66af462e2f79b9018033f800459b8fc5befc2208
sscchan <stephen.s.c.chan@gmail.com>
2017-03-23 21:53:49 -0700

    add test cases

--- 1.6_String_Compression/1.6_StringCompression.js
+++ 1.6_String_Compression/1.6_StringCompression.js
@@ -1,9 +1,12 @@
-function StringCompression() {
+function StringCompression(string) {
   
 }
 
 function testCode() {
-
+  console.log('Testing StringCompression Function');
+  console.assert(StringCompression('aabcccccaaa') === 'a2b1c5a3');
+  console.assert(StringCompression('abcdefg') === 'abcdefg');
+  console.assert(stringCompression('aabcdefg') === 'aabcdefg');
 }
 
 testCode();
